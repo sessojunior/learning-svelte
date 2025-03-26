984b8152ffbf2aece29d53603f09e0c630fd2407
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2025-03-26 17:27:52 -0300

    Atualizado scripts

---
+++ auth-drizzle/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Autenticação com Sveltekit, Drizzle e Better Auth</h1>
+<p>Clique em um dos botões abaixo para prosseguir:</p>
+<p><a href="/signup">Cadastre-se</a> ou <a href="/signin">Login</a></p>
--- delivery/drizzle.config.ts
+++ delivery/drizzle.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+import { defineConfig } from 'drizzle-kit'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set')
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
@@ -11,4 +15,4 @@ export default defineConfig({
 	verbose: true,
 	strict: true,
 	dialect: 'sqlite'
-});
+})
